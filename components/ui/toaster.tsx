c7b3c40b5a243fee1f2c7a04d87100da7439653f
Crispus <crispus.nzano@gmail.com>
2025-04-26 16:37:50 +0300

    fix: toast error

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -10,26 +10,38 @@ import {
   ToastViewport,
 } from "@/components/ui/toast"
 
+type ToastType = {
+  id: string | number
+  title?: React.ReactNode
+  description?: React.ReactNode
+  action?: React.ReactNode
+  [key: string]: any
+}
+
+
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
+  <ToastProvider>
+    {toasts.map(function (toast: ToastType) {
+      const { id, title, description, action, ...props } = toast
+      return (
+        <Toast key={id} {...props}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && (
+              <ToastDescription>{description}</ToastDescription>
+            )}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      )
+    })}
+    <ToastViewport />
+  </ToastProvider>
+)
 }
+
+  
